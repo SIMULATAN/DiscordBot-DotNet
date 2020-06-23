343ed273c6f42cde69e411940a0dc6bb320de688
SIMULATAN <63104422+SIMULATAN@users.noreply.github.com>
2020-06-23 20:51:24 +0200

    Implemented the automatic Channel Creation Module (not working!)

---
+++ DiscordBot.Modules/CommandModules/YouTubeChannelsModule.cs
@@ -0,0 +1,27 @@
+﻿using System.Threading.Tasks;
+using Discord;
+using Discord.Commands;
+using Microsoft.Extensions.Logging;
+
+namespace DiscordBot.Modules.CommandModules
+{
+    public class YouTubeChannelsModule : ModuleBase
+    {
+        private readonly ILogger<PingModule> logger;
+
+        public YouTubeChannelsModule(ILogger<PingModule> logger)
+        {
+            this.logger = logger;
+        }
+
+        [Command("ytsetup")]
+        public async Task YTSetup()
+        {
+            logger.LogInformation($"Kanäle werden auf dem Server {base.Context.Guild.Name} erstellt...");
+            var _category = (ICategoryChannel)base.Context.Guild.CreateCategoryAsync("YouTube Statistiken");
+            var _channel1 = (IVoiceChannel)base.Context.Guild.CreateVoiceChannelAsync("Abonnenten");
+            await _channel1.AddPermissionOverwriteAsync("everyone", ChannelPermission.Connect: PermValue.Deny);
+            await _channel1.ModifyAsync(ChannelPermissions.Voice.Connect: PermValue.Deny);
+        }
+    }
+}
