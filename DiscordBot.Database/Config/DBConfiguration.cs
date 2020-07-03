0a322ae4085e5e5a714eb4ee996f85d050b42c92
SIMULATAN <SIMULATAN>
2020-07-03 16:23:54 +0200

    Implemented the Statusmodule

---
+++ DiscordBot.Database/Config/DBConfiguration.cs
@@ -0,0 +1,15 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DiscordBot.Domain.Database.Config
+{
+    public class DBConfiguration
+    {
+        [JsonProperty(PropertyName = "db_endpoint")]
+        public string DbURL { get; set; }
+        [JsonProperty(PropertyName = "db_key")]
+        public string DbKey { get; set; }
+    }
+}
---
+++ DiscordBot.Modules/CommandModules/UpdateStatusModule.cs
@@ -0,0 +1,101 @@
+﻿using Discord;
+using Discord.Commands;
+using DiscordBot.Modules.Services;
+using SixLabors.ImageSharp;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading;
+using System.Threading.Tasks;
+using Newtonsoft.Json.Linq;
+
+namespace DiscordBot.Modules
+{
+    public class UpdateStatusModule : ModuleBase<SocketCommandContext>
+    {
+        static private int _statusIndex = 0;
+        //private AuthorizationService _authorizationService;
+        static public int _abort = 1;
+
+        static List<string> _statusList = new List<string>() { "CoderDojoCommunityBot!", "den Code der Coder an" };
+        static List<ActivityType> _activityList = new List<ActivityType>() { ActivityType.Playing, ActivityType.Watching };
+
+        //public CustomStatusModule(AuthorizationService authorizationService)
+        //{
+        //    _authorizationService = authorizationService;
+        //}
+        [Command("setstatus")]
+        public async Task Setstatus(int number, ActivityType activity, [Remainder] string input)
+        {
+            //if (_authorizationService.IsUserTrusted(base.Context.User.Username + "#" + base.Context.User.DiscriminatorValue))
+            //{
+                await ReplyAsync($"Der Status {number} wurde auf **{activity} {input}** gesetzt!");
+            if (number > _statusList.Count)
+            {
+                _statusList.Add(input);
+                _activityList.Add(activity);
+            }
+            else
+            {
+                _statusList[number-1] = input;
+                _activityList[number-1] = activity;
+            }
+            //}
+            //else if (!_authorizationService.IsUserTrusted(base.Context.User.Username + "#" + base.Context.User.DiscriminatorValue))
+            //{
+            //    await ReplyAsync($"Du bist nicht getrustet, {base.Context.User.Mention}");
+            //}
+            //else
+            //{
+            //    await ReplyAsync("Ein Fehler ist aufgetreten! Bitte informiere die Admins darüber!");
+            //}
+        }
+        [Command("deletestatus")]
+        public async Task Deletestatus(int number)
+        {
+            //if (useristrusted)
+            //{
+            //    JObject jo = JObject.Parse(_activityList);
+            //    jo[number - 1].Remove();
+                
+            //}
+        }
+        [Command("activity")]
+        public async Task StatusType(string input)
+        {
+            //if (_authorizationService.IsUserTrusted(base.Context.User.Username + "#" + base.Context.User.DiscriminatorValue) && input == "DnD" || input == "Do not Disturb" || input == "Bitte nicht stören")
+            //{
+            //    await base.Context.Client.SetStatusAsync(UserStatus.DoNotDisturb);
+            //    await ReplyAsync("Der Status wurde auf **Bitte nicht stören** gesetzt!");
+            //}
+            //else if (_authorizationService.IsUserTrusted(base.Context.User.Username + "#" + base.Context.User.DiscriminatorValue) && input == "Online" || input == "online")
+            //{
+            //    await base.Context.Client.SetStatusAsync(UserStatus.Online);
+            //    await ReplyAsync("Der Status wurde auf **Online** gesetzt!");
+            //}
+            //else if (_authorizationService.IsUserTrusted(base.Context.User.Username + "#" + base.Context.User.DiscriminatorValue) && input == "AFK" || input == "Afk" || input == "afk")
+            //{
+            //    await base.Context.Client.SetStatusAsync(UserStatus.AFK);
+            //    await ReplyAsync("Der Status wurde auf **Abwesend** gesetzt!");
+            //}
+            //else if (!_authorizationService.IsUserTrusted(base.Context.User.Username + "#" + base.Context.User.DiscriminatorValue))
+            //{
+            //    await ReplyAsync($"Du bist nicht getrustet, {base.Context.User.Mention}");
+            //}
+            //else
+            //{
+            //    await ReplyAsync("Ein Fehler ist aufgetreten! Bitte informiere die Admins darüber!");
+            //}
+        }
+        [Command("ssu")]
+        public async Task StatusUpdater()
+        {
+            while (true)
+            {  
+                await base.Context.Client.SetActivityAsync(new Game(_statusList.ElementAtOrDefault(_statusIndex), type: _activityList.ElementAtOrDefault(_statusIndex)));
+                _statusIndex = _statusIndex + 1 == _statusList.Count ? 0 : _statusIndex + 1;
+                await Task.Delay(TimeSpan.FromSeconds(60/5));
+            }
+        }
+    }
+}
